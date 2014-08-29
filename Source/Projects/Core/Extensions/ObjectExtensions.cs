74561041a737f862d788f18dd116986472a31aac
Torben Rahbek Koch <torben@rahbekkoch.dk>
2014-08-29 17:51:34 +0200

    Ädd file ObjectExtensions.cs.

---
+++ Source/Projects/Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Linq.Expressions;
+
+namespace SoftwarePassion.Common.Core.Extensions
+{
+    /// <summary>
+    /// Useful extensions to object.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Access to a property name on any class type with compile time checked name.
+        /// </summary>
+        /// <typeparam name="TClass">The type of the object instance.</typeparam>
+        /// <typeparam name="TProperty">The type of the property to access.</typeparam>
+        /// <param name="me">The object instance on which to access a property.</param>
+        /// <param name="action">The action.</param>
+        /// <returns>The name of the property.</returns>
+        /// <remarks>This allows you to write code like:
+        /// <code>
+        /// var instance = new MyClass();
+        /// var propertyName = instance.PropName(inst => inst.Property);
+        /// </code>
+        /// </remarks>
+        public static string PropName<TClass, TProperty>(
+            this TClass me, 
+            Expression<Func<TClass, TProperty>> action) 
+            where TClass : class
+        {
+            Contract.Requires(action != null);
+
+            var expression = (MemberExpression)action.Body;
+            string name = expression.Member.Name;
+
+            return name;
+        }
+    }
+}
